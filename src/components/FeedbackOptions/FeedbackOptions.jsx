7dc9d8016ebc6a97b44993e66fb1c2ba841488c7
SemenZvehintsev <kvadratniibomzh@gmail.com>
2023-01-17 10:42:49 +0200

    added options

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types';
 import styles from 'components/FeedbackOptions/FeedbackOptions.module.css';
+import { options } from 'assets/options';
 
 export const FeedbackOptions = ({onLeaveFeedback}) => {
     return <div className={styles.feedback}>
-        <button className={styles.button} type="button" name='Good' onClick={onLeaveFeedback}>Good</button>
-        <button className={styles.button} type="button" name='Neutral' onClick={onLeaveFeedback}>Neutral</button>
-        <button className={styles.button} type="button" name='Bad' onClick={onLeaveFeedback}>Bad</button>
+        {options.map(option => <button className={styles.button} type="button" name={option} onClick={onLeaveFeedback}>{option}</button>)}
     </div>
 }
 
